4ce14d5eec1f13ead7f4150f5123dbcc4187b422
Wright <wrightkhlebisol@gmail.com>
2020-04-09 23:20:56 +0100

    Created function to check if user can add a post

--- class-wright-post-notice-editor.php
+++ class-wright-post-notice-editor.php
@@ -5,6 +5,7 @@ class Wright_Post_Notice_Editor
     public function initialize()
     {
         add_action('add_meta_boxes', [$this, 'add_meta_box']);
+        add_action('save_post' . [$this, 'save_post_notice']);
     }
 
     public function add_meta_box()
@@ -22,5 +23,16 @@ class Wright_Post_Notice_Editor
     public function post_notice_display()
     {
         require_once(plugin_dir_path(__FILE__) . 'views/wright-post-notice-editor.php');
+    }
+
+    public function save_post_notice($post_id)
+    {
+        if (!$this->user_can_save($post_id)) {
+            return;
+        }
+    }
+
+    public function user_can_save($post_id)
+    {
     }
 }
