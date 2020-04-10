5d9a199b24264e8fcd84ca8c0debdf6c1bcc24cb
Wright <wrightkhlebisol@gmail.com>
2020-04-11 00:13:56 +0100

    Added feature to display the post notice on homepage

--- class-wright-post-notice-display.php
+++ class-wright-post-notice-display.php
@@ -13,11 +13,16 @@ class Wright_Post_Notice_Display
         if ('' != $notice) {
             $notice_html = '<div id="wright-post-notice">';
             $notice_html .= $notice;
-            $notice_html .= '</div>';
+            $notice_html .= ' </div>';
 
             $content = $notice_html . $content;
         }
 
         return $content;
+    }
+
+    public function enqueue_styles()
+    {
+        wp_enqueue_style('wright-post-notice', plugins_url('wright-post-notice/assets/css/public.css'), [], '1.0.0');
     }
 }
